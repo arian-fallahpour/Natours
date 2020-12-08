09a92358fbd6818d2d53c1533751a7c265fe2d82
JinXzAF <arianf2004@gmail.com>
2020-12-07 23:04:17 -0500

    Implement stripe implementation 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,15 +9,15 @@ const factory = require("./handlerFactory");
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
+  // console.log(tour);
 
-  // 2) Create the checkout session
-  // doing all this is an API call
+  // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    // success_url: `${req.protocol}://${req.get("host")}/?tour=${
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get("host")}/my-tours`,
+    success_url: `${req.protocol}://${req.get("host")}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -27,8 +27,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         description: tour.summary,
         images: [
           `${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`,
-        ], // These images have to be on the internet
-        amount: tour.price * 100, // price in cents
+        ],
+        amount: tour.price * 100,
         currency: "usd",
         quantity: 1,
       },
@@ -37,7 +37,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   // 3) Create session as response
   res.status(200).json({
-    status: "sucess",
+    status: "success",
     session,
   });
 });
@@ -56,7 +56,9 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
-  await Booking.create({ tour, user, price });
+  console.log(tour, user, price);
+  const lol = await Booking.create({ tour, user, price });
+  console.log(lol);
 };
 
 exports.webhookCheckout = (req, res, next) => {
